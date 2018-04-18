76e8deb078e3643b7086950d9631694368a0f9a0
IamCharlesM <because@charlesis.me>
2018-04-18 16:04:17 -0500

    Product route created

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
