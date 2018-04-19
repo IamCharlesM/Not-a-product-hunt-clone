cc9b61b2ddd014d14caccbf1a3ba2608571ad4bd
IamCharlesM <because@charlesis.me>
2018-04-19 15:18:15 -0500

    Username has already been taken. Error loading.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -6,10 +6,15 @@ def signup(request):
     if request.method == 'POST':
         #Sign up
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.get(username=request.POST['username'])
-            return render(request, 'accounts/signup.html', {'error': 'Username has already been taken.'})
-        else:
-            return render(request, 'accounts/signup.html')
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+    else:
+        return render(request, 'accounts/signup.html')
 
 
 def login(request):
