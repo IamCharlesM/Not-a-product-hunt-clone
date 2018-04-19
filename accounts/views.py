4413ddc9f06c57f5a0c100ded2243c25bce6003f
IamCharlesM <because@charlesis.me>
2018-04-19 15:05:08 -0500

    Accounts urls created, views set up.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        #Sign up
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request, 'accounts/signup.html', {'error': 'Username has already been taken.'})
+        else:
+            return render(request, 'accounts/signup.html')
+
+
+def login(request):
+   return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    #Need to add route to homepage
+   return render(request, 'accounts/signup.html')
