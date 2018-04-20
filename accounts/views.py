7975e9b6130feae60e4782665bffd3bb779e4ea2
IamCharlesM <because@charlesis.me>
2018-04-20 10:20:59 -0500

    Login error message rendering, login fuction working.

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,15 @@ def signup(request):
 
 
 def login(request):
-   return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user =  auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect'})    
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
