b0a5e1c6aa5594860f00a9ea75a5eda507876813
IamCharlesM <because@charlesis.me>
2018-04-20 11:46:52 -0500

    Can no longer go to create page if not logged in

--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    return render(request, 'products/create.html')
