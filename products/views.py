645a83335b1cb925c0e7750664e9088a7f764863
IamCharlesM <because@charlesis.me>
2018-04-23 12:23:02 -0500

    Upvoting fucntion added.

--- products/views.py
+++ products/views.py
@@ -36,3 +36,14 @@ def create(request):
 def detail (request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product':product})
+
+@login_required
+def upvote (request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect ('/products/' + str(product.id))
+    else:
+        pass
+        
